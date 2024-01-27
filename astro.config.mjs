5d7e03cf12f87874346245c5eb76921508f9b4ac
Berzan <berzan@tutanota.com>
2024-01-27 16:03:12 +0000

    update: Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import astroMetaTags from "astro-meta-tags"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://berzan.org',
+    integrations: [mdx({
+        syntaxHighlight: 'shiki',
+        shikiConfig: { theme: 'one-dark-pro' },
+    }), sitemap(), astroMetaTags()],
+})

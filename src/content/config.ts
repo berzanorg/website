20c670288bb235943c84ee270d4727212b6d95d5
Berzan <berzan@tutanota.com>
2024-01-27 19:37:54 +0000

    update: collection definition

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,6 @@ const postsCollection = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.date(),
         keywords: z.array(z.string())
     })
 })

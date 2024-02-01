9c3991f221c684ec8478adaf8012ea22ba1649ed
Berzan <berzan@tutanota.com>
2024-02-01 19:22:46 +0000

    add: pages collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,24 @@
 import { z, defineCollection } from "astro:content"
 
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        keywords: z.array(z.string())
-    })
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		keywords: z.array(z.string()),
+	}),
+})
+
+const pagesCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		keywords: z.array(z.string()),
+	}),
 })
 
 export const collections = {
-    posts: postsCollection,
+	posts: postsCollection,
+	pages: pagesCollection,
 }

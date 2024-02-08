6415ca9e2320d3d9da703738e951b7678499fded
Berzan <berzan@tutanota.com>
2024-02-08 14:43:24 +0000

    extend: font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,12 @@ import typography from "@tailwindcss/typography"
 export default {
 	content: ["./src/**/*.{astro,html}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: '"Lexend", ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+				mono: '"Commitmono", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+			},
+		},
 	},
 	plugins: [typography()],
 }
